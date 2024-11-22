c977ca473668d6771a1f6e8a4fe63aff612c17cf
David van der Tuijn <davidvandertuijn@mac.com>
2024-11-22 22:47:01 +0800

    Update mandrill.php

--- src/config/mandrill.php
+++ src/config/mandrill.php
@@ -2,6 +2,8 @@
 
 return [
 
-    'url' => env('MANDRILL_URL', 'https://mandrillapp.com/api/1.0')
+    'url' => env('MANDRILL_URL', 'https://mandrillapp.com/api/1.0'),
+    'from_email' => env('MAIL_FROM_ADDRESS'),
+    'from_name' => env('MAIL_FROM_NAME') ?? 'Mandrill Mailer.',
 
 ];
